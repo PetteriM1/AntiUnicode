e7cb1d1820ec08231cb0579ebb9c53766508587a
Petteri <26197131+PetteriM1@users.noreply.github.com>
2022-04-07 20:09:07 +0300

    New pattern

--- src/main/java/me/petterim1/antiunicode/Main.java
+++ src/main/java/me/petterim1/antiunicode/Main.java
@@ -6,11 +6,7 @@ import cn.nukkit.event.Listener;
 import cn.nukkit.event.player.PlayerChatEvent;
 import cn.nukkit.plugin.PluginBase;
 
-import java.util.regex.Pattern;
-
 public class Main extends PluginBase implements Listener {
-
-    private static final Pattern pattern = Pattern.compile("[^\\p{L}\\p{N}\\p{P}\\p{Z}\\p{Sm}\\p{Sc}\\p{Sk}]", Pattern.UNICODE_CHARACTER_CLASS);
 
     public void onEnable() {
         getServer().getPluginManager().registerEvents(this, this);
@@ -18,6 +14,6 @@ public class Main extends PluginBase implements Listener {
 
     @EventHandler(priority = EventPriority.LOW, ignoreCancelled = true)
     public void onChat(PlayerChatEvent e) {
-        e.setMessage(pattern.matcher(e.getMessage()).replaceAll("?"));
+        e.setMessage(e.getMessage().replaceAll("[^ -~]", "?"));
     }
 }
